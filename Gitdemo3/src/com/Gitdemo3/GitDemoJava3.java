1f2b8443f260bf9653e20d02eac8c37050247793
KB0863 <kshitijbhatkar77@gmail.com>
2025-07-23 16:59:53 +0530

    kshitij2 branch 2nd merge request.

--- Gitdemo3/src/com/Gitdemo3/GitDemoJava3.java
+++ Gitdemo3/src/com/Gitdemo3/GitDemoJava3.java
@@ -5,7 +5,8 @@ public class GitDemoJava3 {
 	public static void main(String[] args) {
 		System.out.println("GitDemoJava3 for 1st branch");
 		// TODO Auto-generated method stub
-
+System.out.println("kshitij1 Branch 1st merge request");
+System.out.println("kshitij2 Branch 2nd merge request");
 	}
 
 }
